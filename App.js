5dceee7f7b6f6ae7c15a366b2b737eb908ef6142
Amit <amitbhabhis@gmail.com>
2023-06-17 11:32:13 +0200

    react elements

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const element = React.createElement('h1',{
     id:"heading"
 },'Hello React');
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(element); 
+console.log(element);
+
+root.render(element);
+//JSX syntax
+const jsxheading = <h1 id="heading">This is our first JSX heading</h1>
+console.log(jsxheading)
+root.render(jsxheading)
